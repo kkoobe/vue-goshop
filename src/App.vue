215cd37e9f36021995a56050da4669ac05af8563
kkoobe <1124091574@qq.com>
2020-03-01 13:46:17 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <h1>HelloWorld</h1>
+    <h2>HelloWorld</h2>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
